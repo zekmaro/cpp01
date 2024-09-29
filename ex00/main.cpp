0c8fad4826c4aad695355e0fac521cb989c86673
Andrey Arama <anarama@c2r7p4.42vienna.com>
2024-09-29 16:45:56 +0200

    removed not needed header

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,12 +6,11 @@
 /*   By: anarama <anarama@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 15:39:08 by anarama           #+#    #+#             */
-/*   Updated: 2024/09/29 16:44:39 by anarama          ###   ########.fr       */
+/*   Updated: 2024/09/29 16:45:42 by anarama          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
-#include <random>
 
 int main(void) {
 	Zombie testZombie("First Evaluator");
