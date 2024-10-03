1f62b2d2da08aabc47c78d99c56f86c448871d8a
Andrey Arama <anarama@c2r7p4.42vienna.com>
2024-10-03 13:24:28 +0200

    improved

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: anarama <anarama@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 19:44:42 by anarama           #+#    #+#             */
-/*   Updated: 2024/10/03 11:59:19 by anarama          ###   ########.fr       */
+/*   Updated: 2024/10/03 13:23:45 by anarama          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,25 @@
 #include <iostream>
 #include <string>
 
-std::string getUpdatedLine(std::string oldLine, std::string pattern) {
+std::string getUpdatedLine(std::string oldLine, std::string pattern, std::string replace) {
 	std::string newLine;
 	std::string temp_copy;
 	std::size_t	pattern_start_index;
 	std::size_t current_copy_start_index = 0;
 
-	while ((pattern_start_index = oldLine.find(pattern)) != std::string::npos) {
+	std::cout << oldLine << " " << pattern << std::endl;
+	while ((pattern_start_index = oldLine.find(pattern, current_copy_start_index)) != std::string::npos) {
 		temp_copy =  oldLine.substr(current_copy_start_index, pattern_start_index);
 		newLine += temp_copy;
-		newLine += pattern;
-		current_copy_start_index = pattern_start_index + pattern.length();
+		newLine += replace;
+		current_copy_start_index = pattern_start_index + replace.length();
 	}
 	newLine += oldLine.substr(current_copy_start_index);
+	return newLine;
 }
 
 int main(int argc, char **argv) {
-	if (argc != 5) {
+	if (argc != 4) {
 		std::cerr << "Wrong amount of arguments" << std::endl;
 		return 1;
 	}
@@ -44,12 +46,22 @@ int main(int argc, char **argv) {
 		std::cerr << "Failed to open file" << std::endl;
 		return 1;
 	}
+	std::string outputFileName = argv[1];
+	outputFileName += ".replace";
+	std::ofstream outputFile(outputFileName.c_str());
+	if (!outputFile.is_open()) {
+		std::cerr << "Failed to open file" << std::endl;
+		return 1;
+	}
 	// if (argv[2] == argv[3]) { add logic
 		
 	// }
 	// create output stream
 	std::string line;
 	while (std::getline(inputFile, line)) {
-		std::string modifiedLine = getUpdatedLine(line, argv[3]);
+		std::string modifiedLine = getUpdatedLine(line, argv[2], argv[3]);
+		outputFile << modifiedLine << std::endl;
 	}
+	outputFile.close();
+	inputFile.close();
 }
